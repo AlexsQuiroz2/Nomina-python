077271fe17ff7629730008a6ad38b9dca0c921ef
alexs <alexsquiroz054@gmail.com>
2022-07-25 14:59:25 -0500

    Segunda prueba del ejercicio

--- nomina.py
+++ nomina.py
@@ -6,8 +6,8 @@ descuento_p = 4
 
 
 
-documento = int (input ("Numero de Documento: " ))
-nombre = str (input ("Nombres: "))
+documento = str (input ("Numero de Documento: " ))
+nombre = str (input ("Nombres: ")) 
 apellidos = str (input ("Apellidos: "))
 salario = int (input ('Tu salario: '))
 dias = int (input('¿Cuantos dias trabajo?: '))
@@ -15,16 +15,19 @@ dias = int (input('¿Cuantos dias trabajo?: '))
 
 
 valor_dia = salario/30
-valor_total_dia = valor_dia * dias
+valor_total_dia = int (valor_dia * dias)
+valor_total_sub = sub * dias / 30
 valor_descuento_s = salario * 0.04
 valor_descuento_p = salario * 0.04
-valor_total = valor_total_dia + sub - valor_descuento_s - valor_descuento_p
+valor_total = valor_total_dia + valor_total_sub - valor_descuento_s - valor_descuento_p
 
 print ("el señor/a", nombre, apellidos, "Con el numero de documento", documento, "cuenta con los siguentes valores respectivos a su pago de nomina segun los dias trabajados")
 print ("Su salario es de: ", salario)
+print ("El valor del subsidio de transporte es de: ", sub)
 print ("la cantidad de dias que trabajo es de: ",dias )
 print ("El valor que va a recibir por lo dias trabajados: ", valor_total_dia)
 print ("El valor actual del subsidio es de: ", sub)
+print ("Su valor del subsidio de transporte es de: ", valor_total_sub)
 print ("El valor a descontar de salud es de: ", valor_descuento_s)
 print ("El valor a descontar de pensión es de: ", valor_descuento_p)
 print ("Tu total a recibir es: ", valor_total)
